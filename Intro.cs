2e1c9db9a5fe28c1c1fa4a97dbb7cba82b3a26de
Eric Browne <ericbrowne@hotmail.com>
2023-10-04 11:15:40 +0200

    added introclass and calls the method in Program.cs

---
+++ Intro.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace theWorst
+{
+    public class Intro
+    {
+        public void GameIntro()
+        {
+
+            //Intro
+            Console.WriteLine("*GET READY*");
+            Console.WriteLine("***********");
+            Console.WriteLine("***********");
+            Console.WriteLine("**TO PLAY**");
+            Console.WriteLine("***********");
+            Console.WriteLine("***********");
+            Console.WriteLine("***********");
+            Console.WriteLine("***********");
+            Console.WriteLine("***********");
+            Console.WriteLine("***ROCK****");
+            Console.WriteLine("***********");
+            Console.WriteLine("***PAPER***");
+            Console.WriteLine("***********");
+            Console.WriteLine("*SCISSORS**");
+            Console.WriteLine("***********");
+            Console.WriteLine("***********");
+
+            return;
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using theWorst;
+
 Console.WriteLine("Hello, World!");
+
+
+Intro intro = new Intro();
+intro.GameIntro();
