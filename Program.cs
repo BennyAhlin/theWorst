33a8596b162b9c36399ff6e34ec23b7d6e048b83
BennyAhlin <Benny.Ahlin@hotmail.com>
2023-10-04 11:15:42 +0200

    added a counter in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Collections.Concurrent;
+
 Console.WriteLine("Hello, World!");
+
+
+
+
+
+Console.WriteLine("Line 10 starts here!");
+
+for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine("Count: " + i);
+}
+
+
